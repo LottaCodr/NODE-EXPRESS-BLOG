607dd8b1296e33ffd4c2c95acd7c79c0ea0a121f
King-Lota <lottanna47@gmail.com>
2023-12-07 17:41:36 +0100

    Views demo page created

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res)=> {
 console.log(req.url, req.method);
+
+//set header content type
+res.setHeader('Content-Type', 'text/html');
+
+//send html file
+fs.readFile('./views/index.html', (err, data)=>{
+    if(err) {
+        console.log(err);
+        res.end();
+    } else {
+        //since we're returning one value
+        res.end(data);
+    }
+})
 });
+
 
 server.listen(3000, 'localhost', ()=> {
     console.log('Listening for rqeuest on port 3000');
